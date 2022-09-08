581ee085d3bbdcb4e04e9b646f78ae8685d28ded
KhanLewis <khanlewis@hotmail.co.uk>
2022-09-08 11:53:13 +0000

    clean code and format all files

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,39 +1,41 @@
 /**
- * Declares all the variables used to function
- * the game 
+ * Displays all variables used within the javascript code 
+ * to enable the fucntion of the game.
  */
 
- const weapons = document.getElementsByClassName('weapon');
- let battlesWon = document.getElementById('won');
- let battlesLost = document.getElementById('lost');
- let battlesDraw = document.getElementById('draw');
- let playersWeapon;
- let randomWeapon;
- let computersWeapon;
- let displayMessageResult;
- 
- /** 
-  *  Adds event listner to the buttons so the user can select a weapon
-  * upon user clicking a chosen weapon initiates the game function.
+
+const weapons = document.getElementsByClassName('weapon');
+let battlesWon = document.getElementById('won');
+let battlesLost = document.getElementById('lost');
+let battlesDraw = document.getElementById('draw');
+let playersWeapon;
+let randomWeapon;
+let computersWeapon;
+let displayMessageResult;
+let message;
+
+/** 
+ *  Adds event listner to the buttons so the user can select a weapon
+ * upon user clicking a chosen weapon initiates the game function.
  */
- 
- window.addEventListener('DOMContentLoaded', initialiseGame);
- 
- function initialiseGame(){
- for (playersWeapon of weapons) {
-   playersWeapon.addEventListener('click', function () {
-     playersWeapon = this.getAttribute("data-weapons");
-     if (playersWeapon === "sword"){
-       playersWeapon === 0
-     }else if (playersWeapon === "shield"){
-       playersWeapon === 1
-     } else {
-       playersWeapon === 2
-     }
-     playGame(playersWeapon)
-   })
- }
- }
+
+window.addEventListener('DOMContentLoaded', initialiseGame);
+
+function initialiseGame() {
+  for (playersWeapon of weapons) {
+    playersWeapon.addEventListener('click', function () {
+      playersWeapon = this.getAttribute("data-weapons");
+      if (playersWeapon === "sword") {
+        playersWeapon === 0;
+      } else if (playersWeapon === "shield") {
+        playersWeapon === 1;
+      } else {
+        playersWeapon === 2;
+      }
+      playGame(playersWeapon);
+    });
+  }
+}
 
 /**
  * This enables the computer to select a random weapon
@@ -41,69 +43,70 @@
  */
 
 function playGame(playersWeapon) {
-    randomWeapon = Math.floor(Math.random() * weapons.length);
-    if (randomWeapon === 0) {
-      computersWeapon = "sword";
-    }
-    if (randomWeapon === 1) {
-      computersWeapon = "shield";
-    }
-    if (randomWeapon === 2) {
-      computersWeapon = "axe";
-    };
-    console.log(playersWeapon)
-    console.log(computersWeapon)
-    possibleChances();
-    displayMessage();
-    
+  randomWeapon = Math.floor(Math.random() * weapons.length);
+  if (randomWeapon === 0) {
+    computersWeapon = "sword";
   }
+  if (randomWeapon === 1) {
+    computersWeapon = "shield";
+  }
+  if (randomWeapon === 2) {
+    computersWeapon = "axe";
+  }
+  possibleChances();
+  displayMessage(playersWeapon, computersWeapon);
 
-  /**
- * Adds points to battles won/lost or drawn.
+}
+
+/**
+ * Adds points to battles won/lost or drawn, also displays message 
+ * depending on result.
  */
 
 function possibleChances() {
 
-  // A Draw 
+  // A Draw: 
 
   if (playersWeapon === computersWeapon) {
-      ++battlesDraw.innerText;
-      displayMessageResult = `You chose ${playersWeapon} and your opponent chose ${computersWeapon}. ITS A DRAW`
+    ++battlesDraw.innerText;
+    displayMessageResult = `You chose ${playersWeapon} and your opponent chose ${computersWeapon}. ITS A DRAW`;
 
-    // chances for player to win
+    // Chances for player to win:
 
   } else if (playersWeapon === 'axe' && computersWeapon === 'shield') {
-      ++battlesWon.innerText;
-      displayMessageResult = `You chose ${playersWeapon} and your opponent chose ${computersWeapon}. YOU WON!`
+    ++battlesWon.innerText;
+    displayMessageResult = `You chose ${playersWeapon} and your opponent chose ${computersWeapon}. YOU WON!`;
 
   } else if (playersWeapon === 'shield' && computersWeapon === 'sword') {
-      ++battlesWon.innerText;
-      displayMessageResult = `You chose ${playersWeapon} and your opponent chose ${computersWeapon}. YOU WON!`
-     
+    ++battlesWon.innerText;
+    displayMessageResult = `You chose ${playersWeapon} and your opponent chose ${computersWeapon}. YOU WON!`;
+
 
   } else if (playersWeapon === 'sword' && computersWeapon === 'axe') {
-      ++battlesWon.innerText;
-      displayMessageResult = `You chose ${playersWeapon} and your opponent chose ${computersWeapon}. YOU WON!`
-     
-    // chance for computer to win 
+    ++battlesWon.innerText;
+    displayMessageResult = `You chose ${playersWeapon} and your opponent chose ${computersWeapon}. YOU WON!`;
+
+    // Chance for computer to win: 
 
   } else if (playersWeapon === 'sword' && computersWeapon === 'shield') {
-      ++battlesLost.innerText;
-      displayMessageResult = `You chose ${playersWeapon} and your opponent chose ${computersWeapon}. YOU LOST!`
-     
+    ++battlesLost.innerText;
+    displayMessageResult = `You chose ${playersWeapon} and your opponent chose ${computersWeapon}. YOU LOST!`;
+
 
   } else if (playersWeapon === 'shield' && computersWeapon === 'axe') {
-      ++battlesLost.innerText;
-      displayMessageResult = `You chose ${playersWeapon} and your opponent chose ${computersWeapon}. YOU LOST!`
-     
+    ++battlesLost.innerText;
+    displayMessageResult = `You chose ${playersWeapon} and your opponent chose ${computersWeapon}. YOU LOST!`;
+
 
   } else if (playersWeapon === 'axe' && computersWeapon === 'sword') {
-      ++battlesLost.innerText;
-      displayMessageResult = `You chose ${playersWeapon} and your opponent chose ${computersWeapon}. YOU LOST!`
-    
+    ++battlesLost.innerText;
+    displayMessageResult = `You chose ${playersWeapon} and your opponent chose ${computersWeapon}. YOU LOST!`;
+
   }
 }
-function displayMessage(){
-  message = document.getElementById('display-message')
+
+// Displays a message within the game area div.
+function displayMessage() {
+  message = document.getElementById('display-message');
   message.innerText = displayMessageResult;
 }
